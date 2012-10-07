7b2c2e56607297b87a8ed17e047e1c777e09ee92
N. Taylor Mullen <taylor.mullen@gmail.com>
2012-10-07 00:59:41 -0700

    Normalized Mouse based touches and touch based touches

--- ShootR/ShootR/Client/Ship/TouchController.js
+++ ShootR/ShootR/Client/Ship/TouchController.js
@@ -6,17 +6,40 @@
         movementTouch,
         topOffset = HeightOffset($("#shipStats"));
 
+    function HandleStart(touch) {
+        if (!currentTouchID && touch.clientX <= ($("#game").width() / 2)) {
+            currentTouchID = touch.identifier;
+            movementTouchStart = { X: touch.clientX, Y: touch.clientY - topOffset };
+            movementTouch = { X: touch.clientX, Y: touch.clientY - topOffset };
+        }
+    }
+
+    function HandleMove(touch) {
+        if (currentTouchID === touch.identifier) {
+            movementTouch.X = touch.clientX;
+            movementTouch.Y = touch.clientY - topOffset;
+            return true;
+        }
+
+        return false;
+    }
+
+    function HandleStop(touch) {
+        if (currentTouchID === touch.identifier) {
+            currentTouchID = false;
+            return true;
+        }
+
+        return false;
+    }
+
     function TouchStart(e) {
         e.preventDefault();
 
         for (var i = 0; i < e.changedTouches.length; i++) {
             var touch = e.changedTouches[i];
 
-            if (!currentTouchID && touch.offsetX <= ($("#game").width() / 2)) {
-                currentTouchID = touch.identifier;
-                movementTouchStart = { X: touch.clientX, Y: touch.clientY - topOffset };
-                movementTouch = { X: touch.clientX, Y: touch.clientY - topOffset };
-            }
+            HandleStart(touch);
         }
     }
 
@@ -26,9 +49,7 @@
         for (var i = 0; i < e.changedTouches.length; i++) {
             var touch = e.changedTouches[i];
 
-            if (currentTouchID === touch.identifier) {
-                movementTouch.X = touch.clientX;
-                movementTouch.Y = touch.clientY - topOffset;
+            if (HandleMove(touch)) {
                 break;
             }
         }
@@ -40,8 +61,7 @@
         for (var i = 0; i < e.changedTouches.length; i++) {
             var touch = e.changedTouches[i];
 
-            if (currentTouchID === touch.identifier) {
-                currentTouchID = false;
+            if (HandleStop(touch)) {
                 break;
             }
         }
@@ -50,6 +70,40 @@
     canvas.addEventListener('touchstart', TouchStart, false);
     canvas.addEventListener('touchmove', TouchMove, false);
     canvas.addEventListener('touchend', TouchEnd, false);
+
+    var mouseGUIDS = 0,
+        currentGUID = 0;
+
+    function MouseDown(e) {
+        e.preventDefault();
+
+        var touch = e;
+
+        e.identifier = currentGUID = mouseGUIDS++;
+        HandleStart(e);
+    }
+
+    function MouseMove(e) {
+        e.preventDefault();
+
+        var touch = e;
+
+        e.identifier = currentGUID;
+        HandleMove(e);
+    }
+
+    function MouseUp(e) {
+        e.preventDefault();
+
+        var touch = e;
+
+        e.identifier = currentGUID;
+        HandleStop(e);
+    }
+
+    canvas.addEventListener('mousedown', MouseDown, false);
+    canvas.addEventListener('mousemove', MouseMove, false);
+    canvas.addEventListener('mouseup', MouseUp, false);
 
     that.Draw = function () {
         if (currentTouchID) {
